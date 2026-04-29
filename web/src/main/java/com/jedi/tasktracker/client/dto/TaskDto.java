8a3c267ec37bae5c2a16ce1f5fb144cfe12c7e1f
Timeix3 <timeix3@gmail.com>
2026-04-29 18:59:40 +0300

    fix

--- web/src/main/java/com/jedi/tasktracker/client/dto/TaskDto.java
+++ web/src/main/java/com/jedi/tasktracker/client/dto/TaskDto.java
@@ -1,4 +1,9 @@
 package com.jedi.tasktracker.client.dto;
 
 public record TaskDto(
-    Long id, String title, String content, Integer projectId, String projectName, Boolean isSelectedForSprint) {}
+    Long id,
+    String title,
+    String content,
+    Integer projectId,
+    String projectName,
+    Boolean isSelectedForSprint) {}
